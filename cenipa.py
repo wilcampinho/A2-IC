634cc0a6f2ed1645dfcba3d3edb782d5be6d30e1
Wilson Campinho <43688711+wilcampinho@users.noreply.github.com>
2022-11-03 13:09:00 -0300

    Update cenipa.py

--- cenipa.py
+++ cenipa.py
@@ -1,94 +1,93 @@
-# importar bibliotecas
 import streamlit as st
 import pandas as pd
-import pydeck as pdk
+import pickle as pk
+from sklearn.preprocessing import LabelEncoder
+import math
 
+# Retorna o dataframe original
+def read_data(path):
 
-DATA_URL = "ocorrencias_aviacao.csv"
-
-@st.cache
-def load_data():
-    """
-    Carrega os dados de ocorrências aeronáuticas do CENIPA.
-
-    :return: DataFrame com colunas selecionadas.
-    """
-    columns = {
-        'ocorrencia_latitude': 'latitude',
-        'ocorrencia_longitude': 'longitude',
-        'ocorrencia_dia': 'data',
-        'ocorrencia_classificacao': 'classificacao',
-        'ocorrencia_tipo': 'tipo',
-        'ocorrencia_tipo_categoria': 'tipo_categoria',
-        'ocorrencia_tipo_icao': 'tipo_icao',
-        'ocorrencia_aerodromo': 'aerodromo',
-        'ocorrencia_cidade': 'cidade',
-        'investigacao_status': 'status',
-        'divulgacao_relatorio_numero': 'relatorio_numero',
-        'total_aeronaves_envolvidas': 'aeronaves_envolvidas'
-    }
-
-    data = pd.read_csv(DATA_URL, index_col='codigo_ocorrencia')
-    data = data.rename(columns=columns)
-    data.data = data.data + " " + data.ocorrencia_horario
-    data.data = pd.to_datetime(data.data)
-    data = data[list(columns.values())]
+    data = pd.read_csv(path)
 
     return data
 
+# Retorna o modelo ja treinado
+def read_model(path):
 
-# carregar os dados
-df = load_data()
-labels = df.classificacao.unique().tolist()
+    with open(path, mode = 'rb') as f:
+        model = pk.load(f)
 
+    return model
 
-# SIDEBAR
-# Parâmetros e número de ocorrências
-st.sidebar.header("Parâmetros")
-info_sidebar = st.sidebar.empty()    # placeholder, para informações filtradas que só serão carregadas depois
+# Roda o LabelEncoder no dataframe original para equivaler o modelo treinado
+def encode(data):
 
-# Slider de seleção do ano
-st.sidebar.subheader("Ano")
-year_to_filter = st.sidebar.slider('Escolha o ano desejado', 2008, 2018, 2017)
+    label_enc = LabelEncoder()
 
-# Checkbox da Tabela
-st.sidebar.subheader("Tabela")
-tabela = st.sidebar.empty()    # placeholder que só vai ser carregado com o df_filtered
+    for i in (1,2,4,7,11,15,17,21,22):
 
-# Multiselect com os lables únicos dos tipos de classificação
-label_to_filter = st.sidebar.multiselect(
-    label="Escolha a classificação da ocorrência",
-    options=labels,
-    default=["INCIDENTE", 'ACIDENTE']
-)
+        data.iloc[:,i] = label_enc.fit_transform(data.iloc[:,i])
 
-# Informação no rodapé da Sidebar
-st.sidebar.markdown("""
-A base de dados de ocorrências aeronáuticas é gerenciada pelo ***Centro de Investigação e Prevenção de Acidentes 
-Aeronáuticos (CENIPA)***.
-""")
+    return data
 
-# Somente aqui os dados filtrados por ano são atualizados em novo dataframe
-filtered_df = df[(df.data.dt.year == year_to_filter) & (df.classificacao.isin(label_to_filter))]
+# Remove o Y e algumas colunas com valores todos iguais para equivaler o modelo treinado
+def trim_x(data):
+    
+    X_data = data.iloc[:,:]
+    X_data = X_data.drop('Attrition', axis = 1)
+    X_data = X_data.drop('EmployeeCount', axis = 1)
+    X_data = X_data.drop('Over18', axis = 1)
+    X_data = X_data.drop('StandardHours', axis = 1)
 
-# Aqui o placehoder vazio finalmente é atualizado com dados do filtered_df
-info_sidebar.info("{} ocorrências selecionadas.".format(filtered_df.shape[0]))
+    return X_data
 
+# Cria os sliders para o usuario pode escolher os dados a serem usados na previsão
+def user_input(X_data):
+    
+    data = {}
 
+    for col in X_data.columns.to_list():
+    
+      X_data[col] = X_data[col].astype(float, errors = 'raise')
+      data[col] = st.sidebar.slider(col, X_data[col].min(), X_data[col].max(), float(math.floor(X_data[col].mean())), 1.0)
+          
+    return pd.DataFrame(data, index=[0])
 
+# Definindo o main
+def main():
 
-# MAIN
-st.title("CENIPA - Acidentes Aeronáuticos")
-st.markdown(f"""
-            ℹ️ Estão sendo exibidas as ocorrências classificadas como **{", ".join(label_to_filter)}**
-            para o ano de **{year_to_filter}**.
-            """)
+    # Lendo o dataframe original e o modelo treinado
+    data = read_data('./Human_Resources.csv')
+    model = read_model('emp_att.pkl')
 
-# raw data (tabela) dependente do checkbox
-if tabela.checkbox("Mostrar tabela de dados"):
-    st.write(filtered_df)
+    # Rodando o encode e podando o X
+    data = encode(data)
+    X_data = trim_x(data)
 
+    # Estrutura do site
+    st.write(""" # Predição de um empregado possuir atrito # """)
+    
+    st.write('---')
+   
+    st.sidebar.header('Escolha de paramentros para Predição')
 
-# mapa
-st.subheader("Mapa de ocorrências")
-st.map(filtered_df)
+    df = user_input(X_data)
+
+    st.header('Parametros especificados')
+
+    st.write(df)
+
+    st.write('---')
+
+    prediction = model.predict(df)
+
+    st.header('Attrition Prevista')
+
+    st.write(prediction)
+
+    st.write('---')
+
+# Rodando o main
+if __name__=='__main__':
+    
+    main()
